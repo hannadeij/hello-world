5856e20f6a6f3591d3d330d8e90a9828b85acbb3
hannadeij <hanna98@live.nl>
2020-09-18 11:39:04 +0200

    hello_world.py

--- Python/hello_world.py
+++ Python/hello_world.py
@@ -1,5 +1,16 @@
-print("Hello World")
-for  x in "Hello World":
-    print(x)
+def count_letters(c_var):
+    new_var = len(c_var)
+    return new_var
 
-import pandas as pd
+def main():
+    var = "Hanna Deij"
+    print("Hello World")
+    for  x in "Hello World":
+        print(x)
+
+    new_var = count_letters(var)
+    print(new_var)
+
+main()
+
+
